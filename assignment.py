555dd3de23548a53c9a330cdb32f687f9ebd0033
vHast <fedvhast@gmail.com>
2022-04-04 10:53:45 +0300

    Added assignment.py

---
+++ assignment.py
@@ -0,0 +1,24 @@
+'''
+The = sign in the statement
+
+userAge = 0
+
+Means we are assigning the value on the right side of the = sign to
+the variable on the left.
+
+'''
+
+#The statements x = y and y = x have very different meanings in programming
+
+x = 5
+y = 10
+x = y
+print("x = ", x)
+print("y = ", y)
+
+#The output would be like this
+
+'''
+x = 10
+y = 10
+'''
